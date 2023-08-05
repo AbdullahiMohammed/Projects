9901057c440e53b0f72bd7d5d2411471891d134c
AbdullahiMohammed <abdullahiahmedm@yahoo.com>
2023-08-05 08:41:15 +0100

    Deleted some files

--- Python Projects/writefiles.py
+++
@@ -1,8 +0,0 @@
-f = open(r"C:\Users\DELL\Desktop\techstuff\Projects\Python Projects\petnames.txt", "r")
-f_content = f.read()
-f_content_list = f_content.split("\n")
-
-for file in f_content_list:
-    print(file)
-
-# print(f_content_list)
